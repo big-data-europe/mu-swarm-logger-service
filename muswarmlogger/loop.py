b1f0063b6d8936f7e9d856d0ebaaac6602892e19
Cecile Tonglet <cecile.tonglet@gmail.com>
2017-04-04 14:50:55 +0200

    Uses proper Docker environment variables

--- muswarmlogger/loop.py
+++ muswarmlogger/loop.py
@@ -1,5 +1,6 @@
 from aiodockerpy import APIClient
 import asyncio
+from docker.utils.utils import kwargs_from_env
 import logging
 
 from muswarmlogger.events import (
@@ -12,7 +13,8 @@ logger = logging.getLogger(__name__)
 
 async def run_loop(sparql_endpoint=None, debug=False):
     sparql_context = SPARQLClient(sparql_endpoint, prefixes=prefixes)
-    docker_context = APIClient(timeout=5)
+    docker_args = kwargs_from_env()
+    docker_context = APIClient(timeout=5, **docker_args)
     with sparql_context as sparql, docker_context as docker:
         await run_on_startup_subroutines(docker, sparql)
         async for x in docker.events(decode=True):
